5bb855c0b42e9c640aadb201c029ac8d165d7d12
Muhammad Faizan <m.faizan11f@gmail.com>
2025-07-25 12:39:14 +0500

    Final code

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import { io, server, app } from "./Socket/server.js";
 
 dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
