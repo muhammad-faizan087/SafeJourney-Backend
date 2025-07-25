9227bcab3e013a67da6d85e70e09b44834f4254c
Muhammad Faizan <m.faizan11f@gmail.com>
2025-07-25 15:59:38 +0500

    Final code

--- index.js
+++ index.js
@@ -19,15 +19,22 @@ const port = process.env.PORT || 3000;
 app.use(cookieParser());
 app.use(express.json());
 app.use((req, res, next) => {
+  const allowedOrigins = [
+    "https://safe-journey-frontend.vercel.app",
+    "http://localhost:5173",
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://safe-journey-frontend.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
+
   next();
 });
 
